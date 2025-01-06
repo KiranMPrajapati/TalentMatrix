72649cf02df81db37229a263a8e3c50b79dfc8b6
KiranMPrajapati <m.kiran.prajapati@gmail.com>
2025-01-06 18:32:23 +0545

    feat: load testing for add_collection api

--- locustfile.py
+++ locustfile.py
@@ -8,10 +8,10 @@ base_path = os.path.abspath(os.path.join(current_dir, "../../TalentMatrix"))
 class LoadTestUser(HttpUser):
     wait_time = between(1, 3)
 
-    # @task
-    # def test_add_jd_to_database(self):
-    #     """Test the /add_jd_to_database endpoint."""
-    #     self.client.get("/add_jd_to_database", params={"jd_path": os.path.join(base_path, "/data/dataset/jobDescriptions/JD_data.csv")})
+    @task
+    def test_add_jd_to_database(self):
+        """Test the /add_jd_to_database endpoint."""
+        self.client.get("/add_jd_to_database", params={"jd_path": os.path.join(base_path, "/data/dataset/jobDescriptions/JD_data.csv")})
 
     @task
     def test_retrieve_collection(self):
